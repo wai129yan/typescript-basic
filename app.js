bdc6944267ab13aa81a8962ccfc68f7628c8f135
wai129yan <waiyan.msc.yu12@gmail.com>
2025-08-27 00:34:30 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// let username: string = "kyaw";
+// console.log(username);
+// function addTwoNumber(a: number, b: number)
+//     : number | string {
+//     return a + b;
+// }
+// console.log(addTwoNumber(10, 20));
+// console.log(addTwoNumber(100, 200));
+// console.log(addTwoNumber(1000, 2000));
+var username = "kyaw";
+console.log(username);
+var age = 20;
+console.log(age);
+var isMarried = true;
+console.log(isMarried);
+var price = 100;
+console.log(price);
+var user = "john";
+console.log(user);
+var userAge = 20;
+console.log(userAge);
+var userAge2 = "Twenty";
+console.log(userAge2);
+var data = "hello";
+console.log(data);
+data = 100;
+console.log(data);
+data = true;
+console.log(data);
+var item = ["apple", "orange", "banana"];
+console.log(item);
+item.push("mango");
+console.log(item);
---
+++ app.ts
@@ -0,0 +1,51 @@
+
+// let username: string = "kyaw";
+// console.log(username);
+
+// function addTwoNumber(a: number, b: number)
+//     : number | string {
+//     return a + b;
+// }
+// console.log(addTwoNumber(10, 20));
+// console.log(addTwoNumber(100, 200));
+// console.log(addTwoNumber(1000, 2000));
+
+
+
+
+let username: string = "kyaw";
+console.log(username)
+
+let age: number = 20;
+console.log(age)
+
+let isMarried: boolean = true;
+console.log(isMarried)
+
+let price: number | string = 100;
+console.log(price)
+
+let user: string | number = "john";
+console.log(user)
+
+let userAge: number | string = 20;
+console.log(userAge)
+
+let userAge2: number | string = "Twenty";
+console.log(userAge2)
+
+let data: any = "hello";
+console.log(data)
+data = 100;
+console.log(data)
+data = true;
+console.log(data)
+
+
+let item: string[] = ["apple", "orange", "banana"];
+console.log(item)
+item.push("mango");
+console.log(item)
+
+
+
